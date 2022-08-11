1a2dcd324722e3eb31cce0a35cdf285cd0079981
piotrpdev <peterplaczek03@gmail.com>
2022-08-11 01:41:07 +0100

    dark mode

--- src/lib/components/MainNav.svelte
+++ src/lib/components/MainNav.svelte
@@ -3,6 +3,7 @@
   import { isMenuOpen } from '$lib/assets/js/store'
   import NavItem from './NavItem.svelte'
   import HamburgerMenuButton from './HamburgerMenuButton.svelte'
+  import DarkButton from './DarkButton.svelte';
 </script>
 
 <!-- Contents of this file will be used in the header and the responsive hamburger menu. -->
@@ -14,5 +15,6 @@
     </NavItem>
     {/each}
   </ul>
+  <DarkButton />
   <HamburgerMenuButton closeOnly="true" />
 </nav>
