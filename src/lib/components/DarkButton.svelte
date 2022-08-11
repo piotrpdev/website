32ee42cddebb39efbdd17364db5ba555a13abfa9
piotrpdev <peterplaczek03@gmail.com>
2022-08-11 02:10:30 +0100

    Revert "lets make the button do nothing"
    This reverts commit 14824bbc0f884e907ef19ef81acd8a0527d4b1e0.

--- src/lib/components/DarkButton.svelte
+++ src/lib/components/DarkButton.svelte
@@ -1,5 +1,59 @@
 <script>
-  let hello
+    import { onMount } from 'svelte'
+    import SunSvg from './svg/SunSVG.svelte'
+    import MoonSvg from './svg/MoonSvg.svelte'
+    import { browser } from '$app/env'
+
+    const STORAGE_KEY = 'theme'
+    const DARK_PREFERENCE = '(prefers-color-scheme: dark)'
+  
+    const THEMES = {
+      DARK: 'dark',
+      LIGHT: 'light',
+    };
+
+    let currentTheme = THEMES.LIGHT;
+  
+    const prefersDarkThemes = () => window.matchMedia(DARK_PREFERENCE).matches
+
+    const applyTheme = () => {
+        const preferredTheme = prefersDarkThemes() ? THEMES.DARK : THEMES.LIGHT
+        currentTheme = localStorage.getItem(STORAGE_KEY) ?? preferredTheme
+
+        if (currentTheme === THEMES.DARK) {
+        document.body.classList.remove(THEMES.LIGHT)
+        document.body.classList.add(THEMES.DARK)
+        } else {
+        document.body.classList.remove(THEMES.DARK)
+        document.body.classList.add(THEMES.LIGHT)
+        }
+    };
+  
+    const toggleTheme = () => {
+      const stored = localStorage.getItem(STORAGE_KEY)
+  
+      if (stored) {
+        // clear storage
+        localStorage.removeItem(STORAGE_KEY)
+      } else {
+        // store opposite of preference
+        localStorage.setItem(STORAGE_KEY, prefersDarkThemes() ? THEMES.LIGHT : THEMES.DARK)
+      }
+        applyTheme()
+    };
+
+    onMount(() => {
+        if (browser) {
+          applyTheme()
+          window.matchMedia(DARK_PREFERENCE).addEventListener('change', applyTheme)
+        }
+    });
 </script>
 
-<button class="dark-toggle">Hello</button>
+<button class="dark-toggle" on:click={toggleTheme}>
+    {#if currentTheme === THEMES.LIGHT}
+        <SunSvg />
+    {:else}
+        <MoonSvg />
+    {/if}
+</button>
