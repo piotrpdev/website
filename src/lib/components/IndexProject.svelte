0e3c55887e3bc0bd23e64d1be2a4654cc0c0d627
piotrpdev <peterplaczek03@gmail.com>
2022-08-16 22:56:39 +0100

    expanded homepage

---
+++ src/lib/components/IndexProject.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let thumb, link, title, description, school;
+</script>
+
+<div class="index-project">
+    <div class="media-container"><img class="media" alt="project thumbnail" src={thumb} /></div>
+    <div>
+        <h3><a href={link} target="_blank" rel="noopener noreferrer">{title}</a></h3>
+        {#if school}
+            <div class="badge">University Project ({school})</div>
+        {/if}
+        <p>{description}</p>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,79 @@
+<script>
+	import IndexProject from "$lib/components/IndexProject.svelte";
+
+	const projects = [
+		{
+			thumb: "https://github.com/piotrpdev/cao-calculator/raw/main/.github/images/home_dark.png",
+			link: "https://leavingcertcao.xyz/",
+			title: "CAO Calculator",
+			description: "Android app that helps calculate how many CAO points you need to qualify for a course.",
+			school: null
+		},
+		{
+			thumb: "https://github.com/piotrpdev/WeBuy-Cex-Price-Tracker/raw/master/captures/sheet.gif",
+			link: "https://github.com/piotrpdev/WeBuy-Cex-Price-Tracker",
+			title: "WeBuy Cex Price Tracker",
+			description: "A python script that gets the prices of certain Cex products and uploads them to google sheets.",
+			school: null
+		},
+		{
+			thumb: "https://github.com/piotrpdev/WIT-Timetable-Generator/raw/main/.github/example/example.png",
+			link: "https://github.com/piotrpdev/WIT-Timetable-Generator",
+			title: "WIT Timetable Generator",
+			description: "Uses Puppeteer to automatically fill out the WIT Timetable Page and generate a screenshot and PDF of the timetable.",
+			school: null
+		},
+		{
+			thumb: "/static/images/wd1ca1.png",
+			link: "https://peter-wd2ca1.netlify.app/",
+			title: "Sun Tracker™",
+			description: "Website that displays the current position of the sun in a variety of countries.",
+			school: "Semester 2"
+		},
+		{
+			thumb: "/static/images/wd1ca2.png",
+			link: "https://peter-wd1ca2.netlify.app/",
+			title: "Rosslare Tidy Towns",
+			description: "Website dedicated to Rosslare Tidy Towns, a non-profit organization in my town.",
+			school: "Semester 1"
+		}
+	]
+</script>
+
 <svelte:head>
 	<title>Piotr Płaczek's Website</title>
 </svelte:head>
 
-<h1>Welcome</h1>
+<div>
+	<h1>Welcome</h1>
+	
+	<p>
+		This is where I plan to show off my portfolio as I progress through my programming career.
+	
+		Feel free to check out any of the pages available.
+	</p>
+</div>
 
-This is where I plan to show off my portfolio as I progress through my programming career.
+<div>
+	<h2>Who are you?</h2>
+	
+	<p>I am a software/web developer, currently studying at the <a href="https://setu.ie/" target="_blank" rel="noopener noreferrer">South East Technological University</a> in Ireland. I love music and messing around with electronics. Go check out the <a href="/about">About page</a> to learn more.</p>
+</div>
 
-Feel free to check out my projects or any of the other pages available.
+<div>
+	<h2>Any projects?</h2>
+	
+	<p>Yes! These are my most notable ones right now:</p>
+
+	<div class="projects-list">
+		{#each projects as project}
+			<IndexProject {...project} />
+		{/each}
+	</div>
+</div>
+
+<div>
+	<h2>Can I contact you?</h2>
+	
+	<p>Sure, I would love to talk. Check out the <a href="/contact">Contact page</a> to see all of my social media.</p>
+</div>
