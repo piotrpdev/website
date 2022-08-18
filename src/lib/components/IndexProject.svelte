3aba645d7de58f6b583fc90ac553806e75c562d2
piotrpdev <peterplaczek03@gmail.com>
2022-08-18 14:33:51 +0100

    make index projects nicer

--- src/lib/components/IndexProject.svelte
+++ src/lib/components/IndexProject.svelte
@@ -1,8 +1,10 @@
 <script>
+    import { goto } from "$app/navigation";
+
     export let thumb, link, title, description, school;
 </script>
 
-<div class="index-project">
+<div class="index-project" on:click={() => window.open(link, '_blank', "noopener noreferrer")}>
     <div class="media-container"><img class="media" alt="project thumbnail" src={thumb} /></div>
     <div>
         <h3><a href={link} target="_blank" rel="noopener noreferrer">{title}</a></h3>
